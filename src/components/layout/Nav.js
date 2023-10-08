d2879053cdde72cee6306edb863f9e7c7a5cd8b0
Frank Congson <fcongson@gmail.com>
2023-10-08 11:17:15 -0700

    Update responsive nav and footer

--- src/components/layout/Nav.js
+++ src/components/layout/Nav.js
@@ -11,19 +11,21 @@ export const Nav = () => {
       <nav role="navigation" aria-label="main-navigation">
         <Section>
           <Container className="nav">
-            <Link to="/" title="Logo" className="nav__logo">
-              <div className="img" />
-            </Link>
-            {/* Hamburger menu */}
-            <button
-              className={`nav__button ${isActive && "nav__button--active"}`}
-              aria-expanded={isActive}
-              onClick={() => setIsActive(!isActive)}
-            >
-              <span />
-              <span />
-              <span />
-            </button>
+            <div className="nav__brand">
+              <Link to="/" title="Logo" className="nav__logo">
+                <div className="img" />
+              </Link>
+              {/* Hamburger menu */}
+              <button
+                className={`nav__button ${isActive && "nav__button--active"}`}
+                aria-expanded={isActive}
+                onClick={() => setIsActive(!isActive)}
+              >
+                <span />
+                <span />
+                <span />
+              </button>
+            </div>
             <ul className={`nav__menu ${isActive && "nav__menu--active"}`}>
               <li>
                 <Link to="/" className="nav__link">
